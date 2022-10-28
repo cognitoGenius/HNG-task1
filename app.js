0be61a860b6eb483da8894be203f04727fefa093
Tommy, Ubongabasi <tommyubongabasijohn@gmail.com>
2022-10-28 04:38:56 +0100

    Basic server setup

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+// app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res, next) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*')
+    res.status(200).json({
+        "slackUsername": "String",
+        "backend": true,
+        "age": 21,
+        "bio": "I am Tommy, an up and coming software engineer"
+    })
+})
+
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
